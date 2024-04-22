a447fb2f322858d105754b2a84ae6614aa6722cb
svenjamariko <svenja.taft@icloud.com>
2024-04-22 17:27:40 +0200

    Aufgabe62

--- main.py
+++ main.py
@@ -2,19 +2,29 @@ import my_functions
 import my_classes 
 import json
 
-first_name = input("Geben Sie Ihren Vornamen ein:")
-last_name = input("Geben Sie Ihren Nachnamen ein:")
-sex = input("Sind Sie male oder female:")
-age = int(input("Wie alt sind Sie:"))
+print("Daten des Supervisors")
+first_name = input("Geben Sie den Vornamen des Supervisors ein:")
+last_name = input("Geben Sie den Nachnamen des Supervisors ein:")
 
 if __name__ =="__main__":
-    person = my_classes.Person(first_name, last_name, age, sex)
-    my_classes.Person.save(person)
+    supervisor = my_classes.Supervisor(first_name, last_name)
+    my_classes.Supervisor.save(supervisor)
+
+print("Daten des Subjekts")
+first_name = input("Geben Sie den Vornamen des Subjekts ein:")
+last_name = input("Geben Sie den Nachnamen des Subjekts ein:")
+sex = input("Ist das Subjekt male oder female?:")
+age = int(input("Wie alt ist das Subjekt?:"))
+birthdate = str(input("Was ist das Geburtsdatum des Subjekts?:"))
+
+if __name__ =="__main__":
+    subject = my_classes.Subject(first_name, last_name, age, sex, birthdate)
+    my_classes.Subject.save(subject)
 
 experiment_name = input("Geben Sie den Experimentnamen ein:")
-supervisor = input("Geben Sie den Namen des supervisors ein:")
-date = input("Geben sie das datum ein:")
-subject = input("Geben Sie das subject ein:")
+supervisor = input("Geben Sie nochmal den vollen Namen des Supervisors ein:")
+date = input("Geben Sie das Datum ein:")
+subject = input("Geben Sie nochmal den vollen Namen des Subjekts ein:")
 
 if __name__ == "__main__":
     experiment = my_classes.Experiment(experiment_name, supervisor, date, subject)
--- my_classes.py
+++ my_classes.py
@@ -2,19 +2,33 @@ import my_functions
 import json
 
 class Person():
-    def __init__(self, first_name, last_name, age, sex):
+    def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name  
+
+class Subject(Person):
+    def __init__(self, first_name, last_name, age, sex, birthdate):
+        super().__init__(first_name, last_name)
         self.age = age
         self.sex = sex
         self.max_hr = self.estimate_max_hr()
-       
+        self.__birthdate = birthdate
+
     def estimate_max_hr(self):
         return my_functions.estimate_max_hr(self.age,self.sex)
- 
+    
     def save(self):
-        with open("person.json", "a") as outfile: 
+        with open("subject.json", "a") as outfile: 
             json.dump(self.__dict__, outfile)
+    
+class Supervisor(Person):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+    
+    def save(self):
+        with open("supervisor.json", "a") as outfile:
+            json.dump(self.__dict__, outfile)
+    
 
 class Experiment():
     def __init__(self, experiment_name, date, supervisor, subject):
@@ -26,4 +40,3 @@ class Experiment():
     def save(self):
         with open("experiment.json", "a") as outfile: 
             json.dump(self.__dict__, outfile)
-
